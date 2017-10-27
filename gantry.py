abe4602cf53d9685373992a7f5b2cac9115b6ca7
Yajnavalkya Bandyopadhyay <yajnab@gmail.com>
2017-10-27 17:38:25 +0530

    gantry.py: Initial File

---
+++ gantry.py
@@ -0,0 +1,19 @@
+#
+##Gantry Girder Designer
+##Author-Yajnavalkya Bandyopadhyay
+##email-yajnab@gmail.com
+#
+
+print("Gantry Girder Designer")
+print("Author- Yajnavalkya Bandyopadhyay")
+print("email- yajnab@gmail.com")
+
+class gantry:
+	#Load Calculation
+	print("Load Calculation")
+	#Moment Calculation
+	print("Moments")
+	#Shear Force
+	print("Shear Force")
+	#Design Calculation 
+	print("Design Calculation")
