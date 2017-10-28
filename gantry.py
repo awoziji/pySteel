0b71aa4cb431013f15d3fca1988e22c0c14f8c10
Yajnavalkya Bandyopadhyay <yajnab@gmail.com>
2017-10-28 14:12:32 +0530

    gantry.py: Added the constants

--- gantry.py
+++ gantry.py
@@ -12,13 +12,19 @@ import numpy as np
 
 #Constants
 FOS = 1.5 #Factor of Safety
+W_Crane = #Weight of the Crane
+W_Crab = #Weight of the Crab
+Lc = #Length of
+L1 = #Length of Crane
+L = #Length of Gantry warehouse
+c = #W of Crane
+e= #Depth of Rail and half the depth of the girder
+w = #Dead Load
 
 class gantry:
 	#Load Calculation
 	print("Load Calculation")
 	#Vertical Load
-	W_Crane = #Weight of the Crane
-	W_Crab = #Weight of the Crab 	
 	Wt = W_Crane/4 #Maximum Static Wheel Load due to the weight of the crane
 	W1 = (W_Crane+W_Crab)*(Lc-L1)/(2*Lc) #Maximum Static wheel Load due to Crane Load
 	W_Vert_Total = (Wt + W1) * 1.25 #Total Load, 1.25 is multiplied for impact
