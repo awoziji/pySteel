e1924e2d8c549e496a7eadf606568f28d7c65569
yajnab <yajnab@gmail.com>
2018-04-15 20:14:12 +0530

    fbridge.py : Classify it
    Signed-off-by: yajnab <yajnab@gmail.com>

--- foot_bridge/fbridge.py
+++ foot_bridge/fbridge.py
@@ -10,13 +10,13 @@ lload = 4000 #Live Load
 thk_plank = 0.065 #Plank thickness
 w_plank=8000#Load of Plank in N/(m^2)
 
+class fbridge:
+	#Constant Calculations
+	sw_plank = thk_plank*w_plank#Self Load of Plank
+	total_load = sw_plank+lload
+	max_bm = (total_load*span**2)/8 #Maximum Bending moment w*l^2/8
+	max_sf = (total_load*span)/2 #Maximum shear force wl/2
+	avg_ss = max_sf/(thk*1000) #Average Shear Stress
+	avg_ss = 1.5*avg_ss #Maximum Shear Stress
 
-#Constant Calculations
-sw_plank = thk_plank*w_plank#Self Load of Plank
-total_load = sw_plank+lload
-max_bm = (total_load*span**2)/8 #Maximum Bending moment w*l^2/8
-max_sf = (total_load*span)/2 #Maximum shear force wl/2
-avg_ss = max_sf/(thk*1000) #Average Shear Stress
-avg_ss = 1.5*avg_ss #Maximum Shear Stress
-
-mi_plank = (1000*thk**3)/12
+	mi_plank = (1000*thk**3)/12
