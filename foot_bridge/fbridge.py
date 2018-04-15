90816cd76e79d6edfdb5be37bd5a22176936e802
yajnab <yajnab@gmail.com>
2018-04-15 20:33:43 +0530

    fbridge.py : init() Colorama and fix thickness variable
    Signed-off-by: yajnab <yajnab@gmail.com>

--- foot_bridge/fbridge.py
+++ foot_bridge/fbridge.py
@@ -11,12 +11,13 @@ thk_plank = 0.065 #Plank thickness
 w_plank=8000#Load of Plank in N/(m^2)
 
 class fbridge:
+	init()
 	#Constant Calculations
 	sw_plank = thk_plank*w_plank#Self Load of Plank
 	total_load = sw_plank+lload
 	max_bm = (total_load*span**2)/8 #Maximum Bending moment w*l^2/8
 	max_sf = (total_load*span)/2 #Maximum shear force wl/2
-	avg_ss = max_sf/(thk*1000) #Average Shear Stress
+	avg_ss = max_sf/(thk_plank*1000) #Average Shear Stress
 	avg_ss = 1.5*avg_ss #Maximum Shear Stress
 
-	mi_plank = (1000*thk**3)/12
+	mi_plank = (1000*thk_plank**3)/12
