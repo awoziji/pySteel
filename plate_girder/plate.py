8ff270a8da83d62c1c7493fd5e677c7940489a54
Yajnavalkya Bandyopadhyay <yajnab@gmail.com>
2018-04-24 23:38:49 +0530

    plate_girder : Code for Load Calculation
    Signed-off-by: Yajnavalkya Bandyopadhyay <yajnab@gmail.com>

--- plate_girder/plate.py
+++ plate_girder/plate.py
@@ -17,6 +17,25 @@ from PIL import Image, ImageFont, ImageDraw
 #Constants
 FOS = 1.5 #Factor of Safety
 udl = 30 #UDL Superimposed Load in KN
+c_load_n=2 #NUmber of Concentrated Loads
 c_load = 150 #Concentrated Load at 6m from either ends
 c_load_dist = 6 #Distance of loads from either of the ends
 span= 20 #Span of the Girder
+
+
+class plate_girder:
+
+	w_udl = udl*span#UDL Load on Girder
+	F_w_udl = w_udl*FOS	#Factored w_udl
+	print("Superimposed Uniformaly Distributed Load on the Girder Span is", w_udl)
+	print("Factored Superimposed Uniformaly Distributed Load on the Girder Span is", F_w_udl)
+	w_c= c_load_n*c_load #Superimposed Concentrated Load
+	F_w_c= FOS*c_load_n*c_load #Superimposed Concentrated Load
+	print("Concentrated Load on the Span", w_c)
+	print("Factored Concentrated Load on the Span", F_w_c)
+	w_self = (w_udl+w_c)/300
+	F_w_self = FOS*w_self
+	print("Self Load on the Girder Span is", w_self)
+	print("Factored Self Load on the Girder Span is", F_w_self)
+	w_total_udl = (w_self+udl)*FOS
+	print("Total Load on the Girder Span is", w_total_udl)
