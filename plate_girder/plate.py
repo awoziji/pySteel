e0eec05e42f9f5f18b5eabad4169da5554b82604
Yajnavalkya Bandyopadhyay <yajnab@gmail.com>
2018-04-24 23:56:30 +0530

    plate_girder : Calculation of reactions and fix reaction calculations
    Signed-off-by: Yajnavalkya Bandyopadhyay <yajnab@gmail.com>

--- plate_girder/plate.py
+++ plate_girder/plate.py
@@ -17,7 +17,7 @@ from PIL import Image, ImageFont, ImageDraw
 #Constants
 FOS = 1.5 #Factor of Safety
 udl = 30 #UDL Superimposed Load in KN
-#c_load_n=2 #NUmber of Concentrated Loads
+c_load_n=2 #NUmber of Concentrated Loads
 c_load = 150 #Concentrated Load at 6m from either ends
 c_load_dist = 6 #Distance of loads from either of the ends
 span= 20 #Span of the Girder
@@ -30,12 +30,18 @@ class plate_girder:
 	print("Superimposed Uniformaly Distributed Load on the Girder Span is", w_udl)
 	print("Factored Superimposed Uniformaly Distributed Load on the Girder Span is", F_w_udl)
 	w_c= c_load #Superimposed Concentrated Load
-	F_w_c= FOS*c_load_n*c_load #Superimposed Concentrated Load
+	F_w_c= FOS*w_c #Superimposed Concentrated Load
 	print("Concentrated Load on the Span", w_c)
 	print("Factored Concentrated Load on the Span", F_w_c)
-	w_self = (w_udl+w_c)/300
+	w_self = (w_udl+w_c*c_load_n)/300
 	F_w_self = FOS*w_self
 	print("Self Load on the Girder Span is", w_self)
 	print("Factored Self Load on the Girder Span is", F_w_self)
 	w_total_udl = (w_self+udl)*FOS
 	print("Total Load on the Girder Span is", w_total_udl)
+
+	#Calculation of Reactions
+	#Let two reactions be at the supports. Reaction at the one end be Ra and the other end be Rb
+	#Moment on one side(Lets say its B) have the moment as 0. So the reaction will play like
+	Ra = (w_total_udl*0.5*(span**2)+F_w_c*(span-c_load_dist)+F_w_c*(c_load_dist))/span
+	print("The Reaction at Reaction point a be", Ra)
