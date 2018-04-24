dca355dda546e260b0714558baf4d0934f91118a
Yajnavalkya Bandyopadhyay <yajnab@gmail.com>
2018-04-24 22:21:47 +0530

    plate_girder : Add Initial Headers
    Signed-off-by: Yajnavalkya Bandyopadhyay <yajnab@gmail.com>

---
+++ plate_girder/plate.py
@@ -0,0 +1,15 @@
+#
+##Gantry Girder Designer
+##Author-Yajnavalkya Bandyopadhyay
+##B.Tech Project Part 2
+##Supervisor - Mr. Somnath Majumder
+##email-yajnab@gmail.com
+#
+
+print("Plate Girder Designer")
+print("Author- Yajnavalkya Bandyopadhyay")
+print("email- yajnab@gmail.com")
+
+import numpy as np
+from colorama import init, Fore, Style, Back
+from PIL import Image, ImageFont, ImageDraw
