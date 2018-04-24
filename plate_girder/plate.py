a851e175008ab05e25bff34716458c02aab3b21a
Yajnavalkya Bandyopadhyay <yajnab@gmail.com>
2018-04-24 22:48:12 +0530

    plate_girder: Feed with Constant Values
    Signed-off-by: Yajnavalkya Bandyopadhyay <yajnab@gmail.com>

--- plate_girder/plate.py
+++ plate_girder/plate.py
@@ -13,3 +13,10 @@ print("email- yajnab@gmail.com")
 import numpy as np
 from colorama import init, Fore, Style, Back
 from PIL import Image, ImageFont, ImageDraw
+
+#Constants
+FOS = 1.5 #Factor of Safety
+udl = 30 #UDL Superimposed Load in KN
+c_load = 150 #Concentrated Load at 6m from either ends
+c_load_dist = 6 #Distance of loads from either of the ends
+span= 20 #Span of the Girder
